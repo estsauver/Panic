fa5de2825aaa8f88cb288689837935edcddf0c8f
Earl St Sauver <estsauver@gmail.com>
2014-11-15 09:51:22 -0800

    Initial Commit

--- Panic/ViewController.swift
+++ Panic/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var outputLabel: UILabel!
+    
+    @IBAction func panicButtonPressed(sender: UIButton) {
+        outputLabel.text = "Please do  not press this button again"
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
